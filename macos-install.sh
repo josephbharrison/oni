24977122e0c7c13f4a3baffacce18dc34960ef45
Joe Harrison <joseph.harrison@windstream.com>
2023-01-22 19:23:34 -0500

    personalized development environment

--- macos-install.sh
+++ macos-install.sh
@@ -4,200 +4,162 @@
 now=$(date +%s)
 
 # GLOBAL PARAMETERS
-FONTS="hack mononoki go-mono jetbrains-mono sauce-code-pro open-dyslexic"
+FONTS="hack mononoki go-mono jetbrains-mono"
+ASTRONVIM_REPO=https://github.com/AstroNvim/AstroNvim
 MY_REPO=https://github.com/josephbharrison/oni
 
-TMP_DIR=${HOME}/tmp
-SOURCE_DIR=${TMP_DIR}/oni
-CONFIG_DIR=${HOME}/.config
- 
-# nullify output
-function null(){
-    "$@" &> /dev/null
-}
+# install base configuration only
+[[ $1 == "base" ]] && echo "Installing base astronvim only" && BASE_ONLY=true
 
-# Prerequisite checker
+# Verify homebrew install
 function check_prereqs(){
     echo -en "Checking prereqs: "
-    url="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
     res=$(brew list)
     if [[ $? -ne 0 ]]; then
         echo "MISSING"
         echo "Installing homebrew: "
-        /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL ${url})"
+        /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-    null brew update
 }
 
-# fail and exit
 function fail(){
     echo "FAIL"
     exit 1
 }
 
-# OK response
 function ok(){
     echo "OK"
 }
 
-# package installer
-function brewster(){
-    package=$1
-    name=$2
-    [[ -z $name ]] && name=$package
-    res=$(null brew list $package)
-    if [[ $? -ne 0 ]]; then
-        echo -en "Installing $name: "
-        null brew install $package || return 1
-    else
-        echo -en "Updating $name: "
-        null brew upgrade $package || return 1
-    fi
-    return 0
-}
-
-# font installer
 function install_fonts(){
+    # Install fonts
     echo -en "Installing fonts: "
     for font in $FONTS
     do
-        res=$(null brew list --cask font-${font}-nerd-font)
+        res=$(brew list --cask font-${font}-nerd-font &> /dev/null)
         if [[ $? -ne 0 ]]; then
-            [[ -f $HOME/Library/Fonts/${font}* ]] \
-                && null sudo rm -f $HOME/Library/Fonts/${font}*
-            null brew tap homebrew/cask-fonts && 
-            null brew install --cask font-${font}-nerd-font --force || return 1
+            sudo rm -f $HOME/Library/Fonts/${font}* &> /dev/null
+            brew tap homebrew/cask-fonts &> /dev/null && 
+            brew install --cask font-${font}-nerd-font &> /dev/null || return 1
         fi
     done
     return 0
 }
 
-# tmux installer
 function install_tmux(){
-    brewster tmux
-}
-
-# tldr installer
-function install_tldr(){
-    brewster tealdeer tldr
-    null tldr --update
+    # Install tmux
+    res=$(brew list tmux)
+    if [[ $? -ne 0 ]]; then
+        echo -en "Installing tmux: "
+        brew update &> /dev/null && brew install tmux &> /dev/null || return 1
+    else
+        echo -en "Updating tmux: "
+        brew upgrade tmux &> /dev/null || return 1
+    fi
     return 0
 }
 
-# fzf installer
-function install_fzf(){
-    brewster fzf
-}
-
-# stern installer
-function install_stern(){
-    brewster stern
-}
-
-# kubectl installer
-function install_kubectl(){
-    brewster kubectl
-}
-
-
-# wezterm installer
 function install_wezterm(){
     # Install wezterm
-    res=$(null brew list wezterm)
+    res=$(brew list wezterm-nightly)
     if [[ $? -ne 0 ]]; then
         echo -en "Installing wezterm: "
-        null brew install --cask wezterm --force || return 1
+        brew tap wez/wezterm &> /dev/null
+        brew update &> /dev/null && brew install --cask wez/wezterm/wezterm-nightly &> /dev/null || return 1
     else
         echo -en "Updating wezterm: "
-        null brew upgrade --cask wezterm --force || return 1
+        brew upgrade --cask wezterm-nightly --no-quarantine --greedy-latest &> /dev/null | return 1
     fi
     return 0
 }
 
-# neovim installer
 function install_neovim(){
-    brewster nvim neovim
+    # Install dependencies
+    res=$(brew list nvim)
+    if [[ $? -ne 0 ]]; then
+        echo -en "Installing neovim: "
+        brew update &> /dev/null && brew install nvim &> /dev/null || return 1
+    else
+        echo -en "Updating neovim: "
+        brew upgrade nvim &> /dev/null || return 1
+    fi
+    return 0
 }
 
-# Configure Oni
-function configure_oni(){
-    mkdir -p $TMP_DIR
-    echo -en "Configuring oni: "
+# Configure NeoVim
+function configure_environment(){
+    echo -en "Configuring environment: "
+    config_dir=${HOME}/.config
+    extras_dir=${config_dir}/nvim/extras
 
-    # Backup existing configurations
-    [[ -d $CONFIG_DIR ]] && null cp -rf $CONFIG_DIR ${CONFIG_DIR}.${now}.bak
-    mkdir -p $CONFIG_DIR
+    # Backup configuration
+    [[ -d $config_dir ]] && mv -f $config_dir ${config_dir}.${now}.bak
+    mv -f ~/.bash_profile ~/.bash_profile.${now}.bak
 
-    # Download configurations
-    [[ -d $SOURCE_DIR ]] && rm -rf $SOURCE_DIR
-    null git clone --depth=1 $MY_REPO $SOURCE_DIR || return 1
+    # Create config directory
+    mkdir -p $config_dir
 
-    # Configure oni-nvim
-    nvim_site_dir="${HOME}/.local/share/nvim/site"
+    # Backup nvim packages
+    nvim_local_dir=${HOME}/.local/share/nvim
+    nvim_site_dir=${nvim_local_dir}/site
     [[ -d $nvim_site_dir ]] && mv -f $nvim_site_dir ${nvim_site_dir}.${now}.bak
-    [[ -d $CONFIG_DIR/nvim ]] && rm -rf $CONFIG_DIR/nvim
-    cp -r $SOURCE_DIR/nvim $CONFIG_DIR/nvim || return 1
+
+    # Install nvim configuration
+    nvim_config_dir=${config_dir}/nvim
+    if [[ $BASE_ONLY != true ]];then
+        git clone $MY_REPO $nvim_config_dir &> /dev/null || return 1
+    else
+        git clone $ASTRONVIM_REPO $nvim_config_dir &> /dev/null || return 1
+        return 0
+    fi
 
     # Configure tmux
-    [[ -d $CONFIG_DIR/tmux ]] && rm -rf $CONFIG_DIR/tmux
-    mkdir -p $CONFIG_DIR/tmux
-    cp $SOURCE_DIR/tmux/tmux.conf $CONFIG_DIR/tmux/tmux.conf 
-    null git clone https://github.com/tmux-plugins/tpm $CONFIG_DIR/tmux/plugins/tpm
-    null $CONFIG_DIR/tmux/plugins/tpm/bin/install_plugins
+    mkdir -p $config_dir/tmux
+    cp $extras_dir/tmux/tmux.conf $config_dir/tmux/tmux.conf 
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm &> /dev/null || return 1
 
     # Configure wezterm
-    [[ -d $CONFIG_DIR/wezterm ]] && rm -rf $CONFIG_DIR/wezterm
-    mkdir -p $CONFIG_DIR/wezterm
-    cp -f $SOURCE_DIR/wezterm/* $CONFIG_DIR/wezterm/
+    mkdir -p $config_dir/wezterm
+    cp -f $extras_dir/wezterm/* $config_dir/wezterm/
 
     # Configure starship
-    [[ -d $CONFIG_DIR/starship ]] && rm -rf $CONFIG_DIR/starship
-    mkdir -p $CONFIG_DIR/starship
-    cp -f $SOURCE_DIR/starship/* $CONFIG_DIR/starship/
-    cp $SOURCE_DIR/starship/starship.macos $CONFIG_DIR/starship.toml
+    mkdir -p $config_dir/starship
+    cp -f $extras_dir/starship/* $config_dir/starship/
+    cp -r $extras_dir/starship/default.toml $config_dir/starship.toml
 
     # Configure bash
-    [[ -f ~/.bash_profile ]] && mv -f ~/.bash_profile ~/.bash_profile.${now}.bak
-    cp -f $SOURCE_DIR/bash/.bash_profile ${HOME}/.bash_profile
+    cp -f $extras_dir/bash/.bash_profile ${HOME}/.bash_profile
 
     return 0
 }
 
-# Main installer
+function packer_count(){
+    echo $(ps -ef | grep -c "/[p]acker/")
+}
+
+function packer_sync(){
+    # HEADLESS INSTALL
+    echo -en "Updating packages: "
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' &> /dev/null &
+    while [[ $(packer_count) -gt 0 ]]
+    do
+        sleep 1
+    done
+}
+
 function install(){
-    # oni components
-    components="fonts tmux tldr fzf kubectl stern wezterm neovim"
-    for component in $components
+    installers="check_prereqs install_fonts install_tmux install_wezterm install_neovim configure_environment packer_sync"
+    for installer in $installers
     do
-        installers="${installers} install_${component}"
+        $installer && ok || fail
     done
-
-    # initialize installers
-    steps="check_prereqs $installers"
-    for step in $steps
-    do
-        $step && ok || fail
-    done
-
-    if [[ $@ != *"--skip-configure"* ]]; then
-        configure_oni && ok || fail
-    fi
-
-    echo "Installation complete"
 }
 
-# Post install message
-function start(){
-    cat $SOURCE_DIR/images/startup-splash.ansi
-}
+install
 
-# initialize main installer
-install $@
-
-# exit on optional input
-[[ -e $@ ]] && exit 0
-
-# launch wezterm with `getting_started` message
-export MSG="$(start)"; wezterm --config background="{}" --config colors="{background='rgba(0,0,0,0.67)', selection_fg = 'none', selection_bg = 'rgba(50% 50% 50% 50%)',}" start -- bash -c "echo -e '$MSG'; bash"
-
+echo
+echo "Installation complete, run:"
+echo
+echo "  wezterm start nvim +PackerSync"
+echo
 
