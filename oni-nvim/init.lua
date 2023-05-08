2c112c58dd23966b4c0c201642904a139ace3357
Joe Harrison <josephbharrison@gmail.com>
2023-05-07 20:05:04 -0400

    pipeline test

---
+++ oni-nvim/init.lua
@@ -0,0 +1,26 @@
+-- fast loader
+vim.loader.enable()
+
+-- lazy.nvim loader setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load global mappings
+require("core.mappings")
+
+-- Install plugins
+local plugins = require("plugins")
+require("lazy").setup(plugins)
+
+-- Load user preferences
+require("user")
