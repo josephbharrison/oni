619203f7a514628b601123b266c6ed8a4a45d6bf
Joe Harrison <josephbharrison@gmail.com>
2023-05-07 20:27:51 -0400

    pipeline test

---
+++ oni-nvim/lua/plugins/init.lua
@@ -0,0 +1,44 @@
+local modules = {
+  "neoconf",
+  "neodev",
+  "which-key",
+  "colorschemes",
+  "telescope",
+  "treesitter",
+  "treesitter-playground",
+  "neo-tree",
+  "harpoon",
+  "undotree",
+  "fugative",
+  "lspzero",
+  "comment",
+  "friendly-snippets",
+  "arial",
+  "alpha",
+  "better-escape",
+  "bufdelete",
+  "bufferline",
+  "dressing",
+  "gitsigns",
+  "heirline",
+  "indent-blankline",
+  "lspkind",
+  "nvim-dap",
+  "nvim-dap-ui",
+  "nvim-ts-autotag",
+  "nvim-ts-context-commentstring",
+  "smart-splits",
+  "todo-comments",
+  "toggleterm",
+  "twilight",
+  "zen-mode",
+}
+
+local plugins = {}
+
+for i, module in ipairs(modules) do
+    local plugin = require("plugins." .. module)
+    plugins[i] = plugin
+end
+
+return { plugins }
---
+++ oni-nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,6 @@
+return {
+  "folke/zen-mode.nvim",
+  config = function()
+    require("zen-mode").setup()
+  end
+}
