c8bfe6792c7f5767a44e61ec17d0587ec624e355
Joe Harrison <joseph.harrison@windstream.com>
2023-05-08 19:40:47 -0400

    pipeline test

--- nvim/after/plugin/zen-mode.lua
+++ nvim/after/plugin/zen-mode.lua
@@ -25,12 +25,12 @@ local params = {
         },
         twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
         gitsigns = { enabled = true }, -- disables git signs
-        tmux = { enabled = false }, -- disables the tmux statusline
+        tmux = { enabled = true }, -- disables the tmux statusline
         -- this will change the font size on wezterm when in zen mode
         -- See alse also the Plugins/Wezterm section in this projects README
         wezterm = {
             enabled = true,
-            font = "+2", -- (10% increase per step)
+            font = "+3", -- (10% increase per step)
         },
     },
 }
@@ -44,8 +44,14 @@ wk.register({
 
 -- commands
 local commands = {
-    ["toggle"] = { function() params.plugins.twilight.enabled = false; zen.toggle(params) end, desc = "Zen Mode" },
-    ["toggle_twilight"] = { function() params.plugins.twilight.enabled = true; zen.toggle(params) end, desc = "Twilight Zen Mode" },
+    ["toggle"] = { function()
+        params.plugins.twilight.enabled = false;
+        zen.toggle(params)
+    end, desc = "Zen Mode" },
+    ["toggle_twilight"] = { function()
+        params.plugins.twilight.enabled = true;
+        zen.toggle(params)
+    end, desc = "Twilight Zen Mode" },
 }
 
 -- leader mappings
@@ -61,8 +67,4 @@ for k, v in pairs(mappings) do
         vim.keymap.set('n', k, v[1], {desc=v["desc"]})
     end
 end
-
-
-
-
 
