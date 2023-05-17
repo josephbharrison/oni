f134bf6cbeb07dd09c2ee02d3834aa5a8ebda288
Joe Harrison <joseph.harrison@windstream.com>
2023-05-17 17:41:22 -0400

    pipeline test

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -183,7 +183,7 @@ return {
                     mappings = {
                         ["<bs>"] = "navigate_up",
                         ["."] = "set_root",
-                        ["h"] = "toggle_hidden",
+                        ["H"] = "toggle_hidden",
                         ["/"] = "fuzzy_finder",
                         ["D"] = "fuzzy_finder_directory",
                         -- ["D"] = "fuzzy_sorter_directory",
