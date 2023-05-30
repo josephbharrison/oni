0dbcce7629cc8b3d7e83d068cb65a84d41d1aefb
Joe Harrison <joseph.harrison@windstream.com>
2023-05-30 11:57:44 -0400

    pipeline test

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -5,25 +5,121 @@ end
 
 local goPort = randPort()
 
--- -- Transparency setting
--- require("notify").setup {
---     background_colour = "#000000",
--- }
-
 vim.api.nvim_set_hl(0, "Comment", { italic = true })
 
--- Global DAP configuration
+-- Global DAP Setup
 local dap = require "dap"
 require("dap.ext.vscode").load_launchjs()
 
-local placeholders = {
-    ["${file}"] = function(_) return vim.fn.expand "%:p" end,
-    ["${fileBasename}"] = function(_) return vim.fn.expand "%:t" end,
-    ["${fileBasenameNoExtension}"] = function(_) return vim.fn.fnamemodify(vim.fn.expand "%:t", ":r") end,
-    ["${fileDirname}"] = function(_) return vim.fn.expand "%:p:h" end,
-    ["${fileExtname}"] = function(_) return vim.fn.expand "%:e" end,
-    ["${relativeFile}"] = function(_) return vim.fn.expand "%:." end,
-    ["${relativeFileDirname}"] = function(_) return vim.fn.fnamemodify(vim.fn.expand "%:.:h", ":r") end,
+-- Adapters
+dap.adapters.go = { -- go adapter
+    type = "server",
+    port = goPort,
+    executable = {
+        command = "dlv",
+        args = { "dap", "-l", "127.0.0.1:" .. goPort },
+    },
+}
+dap.adapters.python = { -- python adapter
+    type = "executable",
+    command = os.getenv("HOME") .. "/.pyenv/versions/debugpy/bin/python",
+    args = { "-m", "debugpy.adapter" },
+}
+
+-- Configurations
+dap.configurations.go = { -- go configuration
+    {
+        type = 'go',
+        name = 'Debug Current File',
+        request = 'launch',
+        program = '${file}',
+    },
+    {
+        type = 'go',
+        name = 'Attach to Running Process...',
+        request = 'attach',
+        processId = function() require('dap.utils').pick_process(); end
+    },
+}
+dap.configurations.python = { -- python configuration
+	{
+		type = "python",
+		request = "launch",
+		name = "Debug Current File",
+		program = "${file}",
+		pythonPath = function()
+			local venv = os.getenv("VIRTUAL_ENV")
+			if venv then
+				return venv .. "/bin/python"
+			else
+				return "/usr/bin/python"
+			end
+		end,
+	},
+	{
+		type = "python",
+		request = "attach",
+		name = "Attach to remote process...",
+		connect = {
+			host = function()
+				return vim.fn.input("Host: ")
+			end,
+			port = function()
+				return vim.fn.input("Port: ")
+			end,
+		},
+		mode = "remote",
+		pythonPath = function()
+			local venv = os.getenv("VIRTUAL_ENV")
+			if venv then
+				return venv .. "/bin/python"
+			else
+				return "/usr/bin/python"
+			end
+		end,
+	},
+}
+
+local function all_trim(s)
+	return s:match("^%s*(.-)%s*$")
+end
+
+-- Breakpoint def
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Error" })
+
+-- Add pythonPath to user defined configurations
+for _, config in pairs(dap.configurations.python) do
+    if not config.pythonPath then
+        config.pythonPath = function()
+            local venv = os.getenv("VIRTUAL_ENV")
+            if venv then
+                return venv .. "/bin/python"
+            else
+                return "/usr/bin/python"
+            end
+        end
+    end
+end
+
+-- Add dlvFlags for go configs
+for _, config in pairs(dap.configurations.go) do
+    if not config.dlvFlags then
+        config.dlvFlags = {
+            "--output",
+            "__debug_bin_" .. config.name:gsub(" ", "_"),
+        }
+    end
+end
+
+-- Placeholder expansion for launch directives
+local placeholders = { 
+    ["${file}"] = function(_) return vim.fn.expand("%:p") end,
+    ["${fileBasename}"] = function(_) return vim.fn.expand("%:t") end,
+    ["${fileBasenameNoExtension}"] = function(_) return vim.fn.fnamemodify(vim.fn.expand("%:t"), ":r") end,
+    ["${fileDirname}"] = function(_) return vim.fn.expand("%:p:h") end,
+    ["${fileExtname}"] = function(_) return vim.fn.expand("%:e") end,
+    ["${relativeFile}"] = function(_) return vim.fn.expand("%:.") end,
+    ["${relativeFileDirname}"] = function(_) return vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r") end,
     ["${workspaceFolder}"] = function(_) return vim.fn.getcwd() end,
     ["${workspaceFolderBasename}"] = function(_) return vim.fn.fnamemodify(vim.fn.getcwd(), ":t") end,
     ["${env:([%w_]+)}"] = function(match) return os.getenv(match) or "" end,
@@ -36,37 +132,29 @@ for type, _ in pairs(dap.configurations) do
             for key, fn in pairs(placeholders) do
                 filePath = filePath:gsub(key, fn)
             end
-            for line in io.lines(filePath) do
-                local words = {}
-                for word in string.gmatch(line, "[a-zA-Z0-9_]+") do
-                    table.insert(words, word)
+            -- Verify file exists
+            local f = io.open(filePath, "r")
+            if f ~= nil then
+                for line in io.lines(filePath) do
+                    local words = {}
+                    for word in string.gmatch(line, "[^=]+") do
+                        table.insert(words, all_trim(word))
+                    end
+                    if not config.env then
+                        config.env = {}
+                    end
+                    config.env[words[1]] = words[2]
                 end
-                if not config.env then config.env = {} end
-                config.env[words[1]] = words[2]
             end
         end
     end
 end
-
-
-dap.adapters.go = {
-    type = "server",
-    port = goPort,
-    executable = {
-        command = "dlv",
-        args = { "dap", "-l", "127.0.0.1:" .. goPort },
-    },
-}
-
 
 -- which-key labels
 local wk = require("which-key")
 wk.register({
 	D = { name = "DAP"},
 }, { prefix = "<leader>" })
-
-
-
 
 
 -- commands
