5c25ff3a52e7f9e2f2a1e1b0ac7ee41839df4c43
Joe Harrison <joseph.harrison@windstream.com>
2023-06-06 19:37:31 -0400

    pipeline test

--- nvim/lua/plugins/prettier.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "MunifTanjim/prettier.nvim",
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-    },
-    config = function()
-        require("prettier").setup()
-    end
-}
