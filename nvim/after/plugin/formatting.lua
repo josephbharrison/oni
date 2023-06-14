10fa6a3186b928d587320e79328eafa2d6f2d081
Joe Harrison <joseph.harrison@windstream.com>
2023-06-14 11:58:06 -0400

    pipeline test

--- nvim/after/plugin/formatting.lua
+++ nvim/after/plugin/formatting.lua
@@ -1,16 +1,38 @@
 -- Italicize comments
 vim.api.nvim_set_hl(0, "Comment", { italic = true })
 
--- Set indent to 4 with javascript files
+-- Set indent to 4 with javascript and typescript files
 vim.cmd [[
-  autocmd FileType javascript setlocal tabstop=4 shiftwidth=4 softtabstop=4
+  autocmd FileType javascript,javascriptreact,typescript,typescriptreact setlocal tabstop=4 shiftwidth=4 softtabstop=4
 ]]
 
--- Configure Neoformat to use Prettier
+-- Configure Neoformat to use Prettier for js and ts
 vim.g.neoformat_enabled_javascript = {'prettier'}
+vim.g.neoformat_enabled_typescript = {'prettier'}
 
--- Set up Prettier options
+-- Set up Prettier options for js and ts
 vim.g.neoformat_javascript_prettier = {
+  exe = 'prettier',
+  args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
+  stdin = 1
+}
+vim.g.neoformat_typescript_prettier = {
+  exe = 'prettier',
+  args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
+  stdin = 1
+}
+
+-- Configure Neoformat to use Prettier for jsx and tsx
+vim.g.neoformat_enabled_javascriptreact = {'prettier'}
+vim.g.neoformat_enabled_typescriptreact = {'prettier'}
+
+-- Set up Prettier options for jsx and tsx
+vim.g.neoformat_javascriptreact_prettier = {
+  exe = 'prettier',
+  args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
+  stdin = 1
+}
+vim.g.neoformat_typescriptreact_prettier = {
   exe = 'prettier',
   args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
   stdin = 1
@@ -21,18 +43,38 @@ vim.cmd [[
   autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx Neoformat
 ]]
 
--- --
--- --
---
+
+
 -- -- Italicize comments
 -- vim.api.nvim_set_hl(0, "Comment", { italic = true })
 --
 -- -- Set indent to 4 with javascript files
 -- vim.cmd [[
---   autocmd FileType javascript setlocal tabstop=4 shiftwidth=4 softtabstop=4
+--   autocmd FileType javascript,javascriptreact setlocal tabstop=4 shiftwidth=4 softtabstop=4
 -- ]]
+--
+-- -- Configure Neoformat to use Prettier for js
+-- vim.g.neoformat_enabled_javascript = {'prettier'}
+--
+-- -- Set up Prettier options js
+-- vim.g.neoformat_javascript_prettier = {
+--   exe = 'prettier',
+--   args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
+--   stdin = 1
+-- }
+--
+-- -- Configure Neoformat to use Prettier for jsx
+-- vim.g.neoformat_enabled_javascriptreact = {'prettier'}
+--
+-- -- Set up Prettier options for jsx
+-- vim.g.neoformat_javascriptreact_prettier = {
+--   exe = 'prettier',
+--   args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
+--   stdin = 1
+-- }
 --
 -- -- Format on save using BufWritePre
 -- vim.cmd [[
---   autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx Prettier
+--   autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx Neoformat
 -- ]]
+--
