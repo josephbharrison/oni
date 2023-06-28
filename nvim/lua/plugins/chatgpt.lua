7773a0aacb9e9163743eabf476de18e61fdac983
Joe Harrison <joseph.harrison@windstream.com>
2023-06-28 11:44:45 -0400

    pipeline test

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -8,8 +8,9 @@ return {
     },
     config = function ()
         require("chatgpt").setup({
+            -- Either setup the env variable with `export OPENAI_API_KEY=<api_key>`
+            -- ... or use a secure local secret manager, like `pass`:
             -- api_key_cmd = "pass show chatgpt/api",
         })
     end
-
 }
