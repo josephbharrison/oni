1ad3d25404dd28a966bb30358fba869431d882e1
Joe Harrison <joseph.harrison@windstream.com>
2023-09-08 10:32:31 -0400

    rust autofmt

--- nvim/after/plugin/formatting.lua
+++ nvim/after/plugin/formatting.lua
@@ -43,38 +43,13 @@ vim.cmd [[
   autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx Neoformat
 ]]
 
+-- Add cargo fmt for Rust files
+vim.cmd [[
+  autocmd BufWritePre *.rs lua FormatRustFile()
+]]
 
-
--- -- Italicize comments
--- vim.api.nvim_set_hl(0, "Comment", { italic = true })
---
--- -- Set indent to 4 with javascript files
--- vim.cmd [[
---   autocmd FileType javascript,javascriptreact setlocal tabstop=4 shiftwidth=4 softtabstop=4
--- ]]
---
--- -- Configure Neoformat to use Prettier for js
--- vim.g.neoformat_enabled_javascript = {'prettier'}
---
--- -- Set up Prettier options js
--- vim.g.neoformat_javascript_prettier = {
---   exe = 'prettier',
---   args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
---   stdin = 1
--- }
---
--- -- Configure Neoformat to use Prettier for jsx
--- vim.g.neoformat_enabled_javascriptreact = {'prettier'}
---
--- -- Set up Prettier options for jsx
--- vim.g.neoformat_javascriptreact_prettier = {
---   exe = 'prettier',
---   args = {'--stdin-filepath', '%:p', '--tab-width=4', '--single-quote', '--trailing-comma=all', '--print-width=80'},
---   stdin = 1
--- }
---
--- -- Format on save using BufWritePre
--- vim.cmd [[
---   autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx Neoformat
--- ]]
---
+function FormatRustFile()
+  vim.cmd 'write'
+  vim.fn.system('cargo fmt')
+  vim.cmd 'edit!'
+end
