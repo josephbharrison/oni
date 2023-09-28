ef6973c57f016c82a8519dacafb2253195427e83
Joe Harrison <joseph.harrison@windstream.com>
2023-09-27 20:26:36 -0400

    Symbols Outline which-key

--- nvim/after/plugin/bufdelete.lua
+++ nvim/after/plugin/bufdelete.lua
@@ -1,18 +1,3 @@
--- -- Forcibly delete current buffer
--- require('bufdelete').bufdelete(0, true)
--- -- Wipeout buffer number 100 without force
--- require('bufdelete').bufwipeout(100)
--- -- Delete every buffer from buffer 7 to buffer 30 without force
--- require('bufdelete').bufdelete({7, 30})
--- -- Delete buffer matching foo.txt with force
--- require('bufdelete').bufdelete("foo.txt", true)
-
--- -- which-key labels
--- local wk = require("which-key")
--- wk.register({
--- 	b = { name = "Buffer"},
--- }, { prefix = "<leader>" })
-
 -- commands
 local commands = {
     ["close_buffer"] = { function() require("bufdelete").bufdelete(0, false) end, desc = "Close Buffer" },
---
+++ nvim/after/plugin/symbols-outline.lua
@@ -0,0 +1,18 @@
+-- which-key labels
+local wk = require("which-key")
+
+-- Symbols commands
+local commands = {
+	["toggle"] = { ":SymbolsOutline<cr>", desc = "Symbols Outline" },
+}
+
+-- harpoon leader -> function mappings
+local mappings = {
+	["<leader>O"] = commands.toggle,
+}
+
+for k, v in pairs(mappings) do
+	if v then
+		vim.keymap.set('n', k, v[1], {desc=v["desc"]})
+	end
+end
