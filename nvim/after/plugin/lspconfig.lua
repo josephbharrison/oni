49420fd6eb452dfca08c7de7d4cdf58081061d50
Joseph Harrison <joseph.harrison@windstream.com>
2024-11-08 20:45:22 -0500

    testing pipeline

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -49,8 +49,26 @@ lspconfig.helm_ls.setup {
     filetypes = { "helm.yaml" },
     settings = {
         ['helm-ls'] = {
+            logLevel = "info",
+            valuesFiles = {
+                mainValuesFile = "values.yaml",
+                lintOverlayValuesFile = "values.lint.yaml",
+                additionalValuesFilesGlobPattern = "values*.yaml"
+            },
             yamlls = {
+                enabled = true,
+                enabledForFilesGlob = "*.{yaml,yml}",
+                diagnosticsLimit = 50,
+                showDiagnosticsDirectly = false,
                 path = "yaml-language-server",
+                config = {
+                    schemas = {
+                        kubernetes = "templates/**",
+                    },
+                    completion = true,
+                    hover = true,
+                    -- any other config from https://github.com/redhat-developer/yaml-language-server#language-server-settings
+                }
             }
         }
     }
